1014977021f21eb544c477d69053b0581644b4b5
Vadym23 <35969015+Vadym23@users.noreply.github.com>
2020-05-05 20:41:26 +0300

    Add html, css, js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+const totalBalance = document.querySelector('.total__balance');
+const totalMoneyIncome = document.querySelector('.total__money-income');
+const toatalMoneyExpenses = document.querySelector('.total__money-expenses');
+const historyList = document.querySelector('.history__list');
+const form = document.querySelector('#form');
+const operationName = document.querySelector('.operation__name');
+const operationAmount = document.querySelector('.operation__amount');
